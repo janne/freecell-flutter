9169b6bee5d018cfcf339fbb01de01f54cd1402f
Jan Andersson <janandersson@spotify.com>
2023-05-15 00:16:53 +0200

    Green background

--- lib/freecell_game.dart
+++ lib/freecell_game.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
 import 'components/playing_card.dart';
 
 class FreecellGame extends FlameGame {
+  @override
+  Color backgroundColor() => Colors.green;
+
   @override
   Future<void> onLoad() async {
     for (int i = 0; i < 8; i++) {
